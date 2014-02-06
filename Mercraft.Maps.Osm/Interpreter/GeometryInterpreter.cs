b84c29b946040bb46ffbb426bf4f9408cc2d56da
Ilya Builuk <ilya.builuk@gmil.com>
2014-02-06 22:04:05 +0100

    removed geometry collections

--- Mercraft.Maps.Osm/Interpreter/GeometryInterpreter.cs
+++ Mercraft.Maps.Osm/Interpreter/GeometryInterpreter.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using Mercraft.Maps.Core.Geometries;
 using Mercraft.Maps.Osm.Entities;
 
@@ -13,7 +14,6 @@ namespace Mercraft.Maps.Osm.Interpreter
         /// <summary>
         /// Interprets an OSM-object and returns the corresponding geometry.
         /// </summary>
-        /// <returns></returns>
-        public abstract GeometryCollection Interpret(Element element);
+        public abstract IEnumerable<Geometry> Interpret(Element element);
     }
 }
