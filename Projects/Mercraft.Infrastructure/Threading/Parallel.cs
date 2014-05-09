d507504f1b2a14b68c325d347167765d0e893493
Ilya Builuk <ilya.builuk@gmail.com>
2014-05-10 01:40:03 +0200

    added threading framework

---
+++ Projects/Mercraft.Infrastructure/Threading/Parallel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Mercraft.Infrastructure.Threading
+{
+    public sealed class Parallel
+    {
+        public static void ForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (body == null)
+                throw new ArgumentNullException("body");
+
+            ForEach<TSource>(Partitioner.Create(source), body);
+        }
+
+        public static void ForEach<TSource>(Partitioner<TSource> source, Action<TSource> body)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+            if (body == null)
+                throw new ArgumentNullException("body");
+
+            ForEach(source.GetPartitions,  body);
+        }
+
+        private static void ForEach<TSource>(Func<int, IList<IEnumerator<TSource>>> enumerable,
+            Action<TSource> body)
+        {
+            int num = ThreadBase.AvailableProcessors;
+            var tasks = new Task[num];
+            IList<IEnumerator<TSource>> slices = enumerable(num);
+
+            int sliceIndex = -1;
+
+            Action workerMethod = delegate
+            {
+                IEnumerator<TSource> slice =
+                    slices[Interlocked.Increment(ref sliceIndex)];
+
+                while (slice.MoveNext())
+                {
+                    body(slice.Current);
+                }
+            };
+
+            for (int i = 0; i < num; i++)
+            {
+                tasks[i] = Task.Factory.Create(workerMethod);
+                tasks[i].Run();
+            }
+            tasks.WaitAll();
+        }
+    } 
+}
---
+++ Projects/Mercraft.Infrastructure/Threading/TaskFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Mercraft.Infrastructure.Threading
+{
+    public class TaskFactory
+    {
+        public Task Create(Action<Task> action)
+        {
+            return new Task<Task.Unit>(action);
+        }
+
+        public Task Create(Action action)
+        {
+            return new Task<Task.Unit>(action);
+        }
+
+        public Task<T> Create<T>(Func<Task, T> func)
+        {
+            return new Task<T>(func);
+        }
+
+        public Task<T> Create<T>(Func<T> func)
+        {
+            return new Task<T>(func);
+        }
+
+        public Task Create(IEnumerator enumerator)
+        {
+            return new Task<IEnumerator>(enumerator);
+        }
+
+        public Task<T> Create<T>(Type type, string methodName, params object[] args)
+        {
+            return new Task<T>(type, methodName, args);
+        }
+
+        public Task<T> Create<T>(object that, string methodName, params object[] args)
+        {
+            return new Task<T>(that, methodName, args);
+        }
+    }
+}
