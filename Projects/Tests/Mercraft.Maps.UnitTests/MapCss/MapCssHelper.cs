4608fd8f07434fdf109313d771abdb526d3713b6
Ilya Builuk <ilya.builuk@gmail.com>
2014-05-31 19:16:55 +0200

    added missing files

---
+++ Projects/Tests/Mercraft.Maps.UnitTests/MapCss/MapCssHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Mercraft.Core;
+using Mercraft.Core.MapCss;
+using Mercraft.Core.MapCss.Domain;
+using Mercraft.Core.Scene.Models;
+
+namespace Mercraft.Maps.UnitTests.MapCss
+{
+    public static class MapCssHelper
+    {
+        public static Stylesheet GetStylesheet(string content)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(content);
+            writer.Flush();
+            stream.Position = 0;
+            var provider = new StylesheetProvider(stream);
+            return provider.Get();
+        }
+
+        public static Area GetArea(params KeyValuePair<string, string>[] tags)
+        {
+            return new Area()
+            {
+                Points = new[]
+                {
+                    new GeoCoordinate(0, 0),
+                    new GeoCoordinate(1, 0),
+                    new GeoCoordinate(1, 0),
+                    new GeoCoordinate(0, 0),
+                },
+                Tags = tags
+            };
+        }
+
+        public static Way GetWay(params KeyValuePair<string, string>[] tags)
+        {
+            return new Way()
+            {
+                Points = new[]
+                {
+                    new GeoCoordinate(0, 0),
+                    new GeoCoordinate(1, 0),
+                    new GeoCoordinate(1, 0),
+                    new GeoCoordinate(0, 0),
+                },
+                Tags = tags
+            };
+        }
+    }
+}
---
+++ Projects/Tests/Mercraft.Maps.UnitTests/Zones/Stubs/TestFlatModelBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mercraft.Core;
+using Mercraft.Core.MapCss.Domain;
+using Mercraft.Core.Scene.Models;
+using Mercraft.Core.Unity;
+using Mercraft.Explorer.Builders;
+using Mercraft.Explorer.Helpers;
+using Mercraft.Infrastructure.Dependencies;
+
+namespace Mercraft.Maps.UnitTests.Zones.Stubs
+{
+    public class TestFlatModelBuilder : ModelBuilder
+    {
+        [Dependency]
+        public TestFlatModelBuilder(IGameObjectFactory goFactory)
+            : base(goFactory)
+        {
+        }
+
+        public override IGameObject BuildArea(GeoCoordinate center, Rule rule, Area area)
+        {
+            base.BuildArea(center, rule, area);
+            IGameObject gameObjectWrapper = _goFactory.CreateNew("");
+            BuildModel(center, gameObjectWrapper, rule, area.Points.ToList());
+            var floor = rule.GetZIndex();
+            return gameObjectWrapper;
+        }
+
+        public override IGameObject BuildWay(GeoCoordinate center, Rule rule, Way way)
+        {
+            base.BuildWay(center, rule, way);
+            IGameObject gameObjectWrapper = _goFactory.CreateNew("");
+            BuildModel(center, gameObjectWrapper, rule, way.Points.ToList());
+            var width = rule.GetWidth();
+            var zIndex = rule.GetZIndex();
+            return gameObjectWrapper;
+        }
+
+        private void BuildModel(GeoCoordinate center, IGameObject gameObject, Rule rule,
+            IList<GeoCoordinate> coordinates)
+        {
+        }
+    }
+}
