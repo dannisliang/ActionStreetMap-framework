67829d12a11086b2392e04961f62ccb07d31f1db
Ilya Builuk <ilya.builuk@gmail.com>
2014-06-07 17:06:24 +0200

    osm: code style adjustments

--- Projects/Mercraft.Maps.Osm/Formats/Pbf/PbfReader.cs
+++ Projects/Mercraft.Maps.Osm/Formats/Pbf/PbfReader.cs
@@ -1,22 +1,4 @@
-﻿// OsmSharp - OpenStreetMap (OSM) SDK
-// Copyright (C) 2013 Abelshausen Ben
-// 
-// This file is part of OsmSharp.
-// 
-// OsmSharp is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-// 
-// OsmSharp is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with OsmSharp. If not, see <http://www.gnu.org/licenses/>.
-
-using System;
+﻿using System;
 using System.IO;
 using Ionic.Zlib;
 using ProtoBuf;
@@ -24,17 +6,17 @@ using ProtoBuf;
 namespace Mercraft.Maps.Osm.Formats.Pbf
 {
     /// <summary>
-    /// Reads PBF files.
+    ///     Reads PBF files.
     /// </summary>
     internal class PbfReader
     {
         /// <summary>
-        /// The stream containing the PBF data.
+        ///     The stream containing the PBF data.
         /// </summary>
         private readonly Stream _stream;
 
         /// <summary>
-        /// Creates a new PBF reader.
+        ///     Creates a new PBF reader.
         /// </summary>
         /// <param name="stream"></param>
         public PbfReader(Stream stream)
@@ -43,7 +25,7 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
         }
 
         /// <summary>
-        /// Closes this reader.
+        ///     Closes this reader.
         /// </summary>
         public void Dispose()
         {
@@ -51,18 +33,18 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
         }
 
         /// <summary>
-        /// Moves to the next primitive block, returns null at the end.
+        ///     Moves to the next primitive block, returns null at the end.
         /// </summary>
         /// <returns></returns>
         public PrimitiveBlock MoveNext()
         {
             PrimitiveBlock block = null;
-            int length;
             bool notFoundBut = true;
             while (notFoundBut)
-            { 
+            {
                 // continue if there is still data but not a primitiveblock.
                 notFoundBut = false; // not found.
+                int length;
                 if (Serializer.TryReadLengthPrefix(_stream, PrefixStyle.Fixed32, out length))
                 {
                     // TODO: remove some of the v1 specific code.
@@ -73,7 +55,7 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
                     // I'm just being lazy and re-using something "close enough" here
                     // note that v2 has a big-endian option, but Fixed32 assumes little-endian - we
                     // actually need the other way around (network byte order):
-                    length = IntLittleEndianToBigEndian((uint)length);
+                    length = IntLittleEndianToBigEndian((uint) length);
 
                     BlockHeader header;
                     // again, v2 has capped-streams built in, but I'm deliberately
@@ -91,11 +73,13 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
                     // construct the source stream, compressed or not.
                     Stream sourceStream = null;
                     if (blob.zlib_data == null)
-                    { // use a regular uncompressed stream.
+                    {
+                        // use a regular uncompressed stream.
                         sourceStream = new MemoryStream(blob.raw);
                     }
                     else
-                    { // construct a compressed stream.
+                    {
+                        // construct a compressed stream.
                         var ms = new MemoryStream(blob.zlib_data);
                         sourceStream = new ZLibStreamWrapper(ms);
                     }
@@ -123,14 +107,17 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
         // 4-byte number
         private static int IntLittleEndianToBigEndian(uint i)
         {
-            return (int)(((i & 0xff) << 24) + ((i & 0xff00) << 8) + ((i & 0xff0000) >> 8) + ((i >> 24) & 0xff));
+            return (int) (((i & 0xff) << 24) + ((i & 0xff00) << 8) + ((i & 0xff0000) >> 8) + ((i >> 24) & 0xff));
         }
     }
 
-    abstract class InputStream : Stream
+    internal abstract class InputStream : Stream
     {
+        private long _pos;
+
         protected abstract int ReadNextBlock(byte[] buffer, int offset, int count);
-        public sealed override int Read(byte[] buffer, int offset, int count)
+
+        public override sealed int Read(byte[] buffer, int offset, int count)
         {
             int bytesRead, totalRead = 0;
             while (count > 0 && (bytesRead = ReadNextBlock(buffer, offset, count)) > 0)
@@ -138,86 +125,93 @@ namespace Mercraft.Maps.Osm.Formats.Pbf
                 count -= bytesRead;
                 offset += bytesRead;
                 totalRead += bytesRead;
-                pos += bytesRead;
+                _pos += bytesRead;
             }
             return totalRead;
         }
-        long pos;
+
         public override void Write(byte[] buffer, int offset, int count)
         {
             throw new NotImplementedException();
         }
+
         public override void SetLength(long value)
         {
             throw new NotImplementedException();
         }
+
         public override long Position
         {
-            get
-            {
-                return pos;
-            }
-            set
-            {
-                if (pos != value) throw new NotImplementedException();
-            }
+            get { return _pos; }
+            set { if (_pos != value) throw new NotImplementedException(); }
         }
+
         public override long Length
         {
             get { throw new NotImplementedException(); }
         }
+
         public override void Flush()
         {
             throw new NotImplementedException();
         }
+
         public override bool CanWrite
         {
             get { return false; }
         }
+
         public override bool CanRead
         {
             get { return true; }
         }
+
         public override bool CanSeek
         {
             get { return false; }
         }
+
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotImplementedException();
         }
     }
-    class ZLibStreamWrapper : InputStream
-    { 
-        private ZlibStream reader; 
+
+    internal class ZLibStreamWrapper : InputStream
+    {
+        private readonly ZlibStream _reader;
+
         public ZLibStreamWrapper(Stream stream)
         {
-            reader = new ZlibStream(stream, CompressionMode.Decompress);
-        }
-        protected override int ReadNextBlock(byte[] buffer, int offset, int count)
-        {
-            return reader.Read(buffer, offset, count);
+            _reader = new ZlibStream(stream, CompressionMode.Decompress);
         }
 
+        protected override int ReadNextBlock(byte[] buffer, int offset, int count)
+        {
+            return _reader.Read(buffer, offset, count);
+        }
     }
+
     // deliberately doesn't dispose the base-stream    
-    class LimitedStream : InputStream
+    internal class LimitedStream : InputStream
     {
-        private Stream stream;
-        private long remaining;
+        private readonly Stream _stream;
+        private long _remaining;
+
         public LimitedStream(Stream stream, long length)
         {
             if (length < 0) throw new ArgumentOutOfRangeException("length");
             if (stream == null) throw new ArgumentNullException("stream");
             if (!stream.CanRead) throw new ArgumentException("stream");
-            this.stream = stream;
-            this.remaining = length;
+            _stream = stream;
+            _remaining = length;
         }
+
         protected override int ReadNextBlock(byte[] buffer, int offset, int count)
         {
-            if (count > remaining) count = (int)remaining;
-            int bytesRead = stream.Read(buffer, offset, count);
-            if (bytesRead > 0) remaining -= bytesRead;
+            if (count > _remaining) count = (int) _remaining;
+            int bytesRead = _stream.Read(buffer, offset, count);
+            if (bytesRead > 0) _remaining -= bytesRead;
             return bytesRead;
         }
     }
