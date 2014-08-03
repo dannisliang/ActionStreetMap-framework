3e616f3d8371d4ff02b5f3f81ef61ad6fb90663c
Ilya Builuk <ilya.builuk@gmail.com>
2014-08-04 00:23:22 +0200

    Multiplayer: added initial state

---
+++ Projects/Mercraft.Multiplayer/ConnectionManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+
+namespace Mercraft.Multiplayer
+{
+    public class ConnectionManager
+    {
+        public void Connect()
+        {
+            var hubConnection = new HubConnection("http://www.mercraft.com/");
+            IHubProxy gameHubProxy = hubConnection.CreateHubProxy("GameHub");
+        }
+    }
+}
