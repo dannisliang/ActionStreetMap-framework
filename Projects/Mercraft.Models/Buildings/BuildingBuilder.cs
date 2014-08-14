1df082a677eee429978a267beba04e4eff69b1e2
Ilya Builuk <ilya.builuk@gmail.com>
2014-08-14 23:08:17 +0200

    Buildings: use different gameObjects for roof and facade

--- Projects/Mercraft.Models/Buildings/BuildingBuilder.cs
+++ Projects/Mercraft.Models/Buildings/BuildingBuilder.cs
@@ -15,37 +15,31 @@ namespace Mercraft.Models.Buildings
             var facadeMeshData = style.Facade.Builder.Build(building, style);
             var roofMeshData = style.Roof.Builder.Build(building, style);
 
-            // roof triangles calculated starting from 0, so we need to add offset
-            var trisOffset = facadeMeshData.Vertices.Length;
-            for (int i = 0; i < roofMeshData.Triangles.Length; i++)
-                roofMeshData.Triangles[i] += trisOffset;
-
             var gameObject = building.GameObject.GetComponent<GameObject>();
-            
+
+            // NOTE use different gameObject to support different materials
+            AttachChildGameObject(gameObject, "facade", facadeMeshData);
+            AttachChildGameObject(gameObject, "roof", roofMeshData);
+        }
+
+        private void AttachChildGameObject(GameObject parent, string name, MeshData meshData)
+        {
+            var gameObject = new GameObject(name);
+            gameObject.transform.parent = parent.transform;
+
             var mesh = new Mesh();
-            mesh.vertices = Merge(facadeMeshData.Vertices, roofMeshData.Vertices);
-            mesh.triangles = Merge(facadeMeshData.Triangles, roofMeshData.Triangles);
-            mesh.uv = Merge(facadeMeshData.UV, roofMeshData.UV);
+            mesh.vertices = meshData.Vertices;
+            mesh.triangles = meshData.Triangles;
+            mesh.uv = meshData.UV;
             mesh.RecalculateNormals();
 
             var mf = gameObject.AddComponent<MeshFilter>();
 
             var renderer = gameObject.AddComponent<MeshRenderer>();
-            renderer.material = Resources.Load<Material>(style.Facade.Material);
-            renderer.material.mainTexture = Resources.Load<Texture>(style.Facade.Texture);
+            renderer.material = Resources.Load<Material>(meshData.Material);
+            renderer.material.mainTexture = Resources.Load<Texture>(meshData.Texture);
 
             mf.mesh = mesh;
-
-            gameObject.AddComponent<MeshCollider>();
-        }
-
-        private T[] Merge<T>(T[] source1, T[] source2)
-        {
-            T[] result = new T[source1.Length + source2.Length];
-            source1.CopyTo(result, 0);
-            source2.CopyTo(result, source1.Length);
-         
-            return result;
         }
     }
 }
