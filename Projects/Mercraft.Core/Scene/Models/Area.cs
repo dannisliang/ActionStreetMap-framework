de246d9c079aa44867968e83c486ff55c299ba34
Ilya Builuk <ilya.builuk@gmail.com>
2014-08-26 23:46:55 +0200

    Osm: basic processing of multipolygon

--- Projects/Mercraft.Core/Scene/Models/Area.cs
+++ Projects/Mercraft.Core/Scene/Models/Area.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections.Generic;
-
-namespace Mercraft.Core.Scene.Models
+﻿namespace Mercraft.Core.Scene.Models
 {
     /// <summary>
-    /// Represents connected polygon. Used for buildings, parks
+    ///     Represents connected polygon. Used for buildings, parks
     /// </summary>
-    public class Area: Model
+    public class Area : Model
     {
         public GeoCoordinate[] Points { get; set; }
 
+        public GeoCoordinate[] Holes { get; set; }
+
         public override bool IsClosed
         {
-            get { return
-                Points.Length > 2 &&
-                Points[0] == Points[Points.Length - 1]; }
+            get
+            {
+                return
+                    Points.Length > 2 &&
+                    Points[0] == Points[Points.Length - 1];
+            }
         }
     }
 }
--- Projects/Mercraft.Maps.Osm/Visitors/RelationVisitor.cs
+++ Projects/Mercraft.Maps.Osm/Visitors/RelationVisitor.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Mercraft.Core;
 using Mercraft.Core.Scene;
+using Mercraft.Core.Scene.Models;
 using Mercraft.Core.Utilities;
 using Mercraft.Maps.Osm.Entities;
+using Way = Mercraft.Maps.Osm.Entities.Way;
 
 namespace Mercraft.Maps.Osm.Visitors
 {
     public class RelationVisitor : ElementVisitor
     {
-        //private readonly IScene _scene;
+        private readonly IScene _scene;
         public RelationVisitor(IScene scene) : base(scene)
         {
-            // _scene = scene;
+            _scene = scene;
         }
 
         public override void VisitRelation(Relation relation)
         {
+            // see http://wiki.openstreetmap.org/wiki/Relation:multipolygon
 
-            /* if (relation.Tags.ContainsKeyValue("type", "multipolygon") ||
-                relation.Tags.ContainsKeyValue("type", "building"))
+            if (relation.Tags.ContainsKeyValue("type", "multipolygon"))
             {
-                var ways = new List<KeyValuePair<bool, Way>>();
+                var innerWays = new List<Way>();
+                var outerWays = new List<Way>();
                 foreach (var member in relation.Members)
                 {
                     var way = member.Member as Way;
@@ -30,15 +34,30 @@ namespace Mercraft.Maps.Osm.Visitors
                         continue;
 
                     if (member.Role == "inner")
-                    {
-                        ways.Add(new KeyValuePair<bool, Way>(false, way));
-                    }
+                        innerWays.Add(way);
                     else if(member.Role == "outer")
-                    {
-                        ways.Add(new KeyValuePair<bool, Way>(true, way));
-                    }
+                        outerWays.Add(way);
                 }
-            }*/
+
+                if (!outerWays.Any())
+                    return;
+                
+                var points = new List<GeoCoordinate>();
+                foreach (var outerWay in outerWays)
+                {
+                    points.AddRange(outerWay.GetPoints());
+                }              
+
+                // TODO process inner points!
+
+                var area = new Area
+                {
+                    Id = relation.Id,
+                    Points = points.ToArray(),
+                    Tags = relation.Tags
+                };
+                _scene.AddArea(area);
+            }
         }
     }
 }
