41fbff6a96d2bee659dd985dad6d15d4ed227244
Ilya Builuk <ilya.builuk@gmail.com>
2014-09-09 00:38:54 +0200

    Refactoring: extracted simple scan line algorithm for roads

---
+++ Projects/Mercraft.Models/Utils/Geometry/SimpleScanLine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Mercraft.Core;
+
+namespace Mercraft.Models.Utils.Geometry
+{
+    public class SimpleScanLine
+    {
+        private static List<int> _scanListBuffer = new List<int>(2);
+        /// <summary>
+        ///     This is optimized version for particular case of algorithm for road segments
+        /// </summary>
+        public static void Fill(MapPoint[] mapPointBuffer, int size, Action<int,int,int> fillAction)
+        {
+            var scanLineStart = int.MaxValue;
+            var scanLineEnd = int.MinValue;
+
+            // search start and end values
+            for (int i = 0; i < mapPointBuffer.Length; i++)
+            {
+                var point = mapPointBuffer[i];
+
+                if (point.Y <= 0 || point.Y >= size)
+                    continue;
+
+                if (scanLineEnd < point.Y)
+                    scanLineEnd = (int)point.Y;
+                if (scanLineStart > point.Y)
+                    scanLineStart = (int)point.Y;
+            }
+
+            for (int z = scanLineStart; z <= scanLineEnd; z++)
+            {
+                for (int i = 0; i < mapPointBuffer.Length; i++)
+                {
+                    var currentIndex = i;
+                    var nextIndex = i == mapPointBuffer.Length - 1 ? 0 : i + 1;
+
+                    if ((mapPointBuffer[currentIndex].Y > z && mapPointBuffer[nextIndex].Y > z) || // above
+                      (mapPointBuffer[currentIndex].Y < z && mapPointBuffer[nextIndex].Y < z)) // below
+                        continue;
+
+                    var start = mapPointBuffer[currentIndex].X < mapPointBuffer[nextIndex].X ?
+                          mapPointBuffer[currentIndex]
+                        : mapPointBuffer[nextIndex];
+
+                    var end = mapPointBuffer[currentIndex].X < mapPointBuffer[nextIndex].X
+                        ? mapPointBuffer[nextIndex]
+                        : mapPointBuffer[currentIndex];
+
+                    var x1 = start.X;
+                    var y1 = start.Y;
+                    var x2 = end.X;
+                    var y2 = end.Y;
+
+                    var d = Math.Abs(y2 - y1);
+
+                    if (Math.Abs(d) < float.Epsilon)
+                        continue;
+
+                    float tanBeta = Math.Abs(x1 - x2) / d;
+
+                    var b = Math.Abs(y1 - z);
+                    var length = b * tanBeta;
+
+                    var x = (int)(x1 + Math.Floor(length));
+
+                    if (x >= size) x = size - 1;
+                    if (x < 0) x = 0;
+
+                    _scanListBuffer.Add(x);
+                }
+
+                if (_scanListBuffer.Count > 1)
+                {
+                    _scanListBuffer.Sort();
+                    fillAction(z, _scanListBuffer[0], _scanListBuffer[_scanListBuffer.Count - 1]);
+                }
+
+                _scanListBuffer.Clear();
+            }
+        }
+    }
+}
--- Projects/Mercraft.Models/Utils/HeightMapProcessor.cs
+++ Projects/Mercraft.Models/Utils/HeightMapProcessor.cs
@@ -20,11 +20,7 @@ namespace Mercraft.Models.Utils
         private float[,] _data;
         private float _ratio;
 
-        private int _scanLineStart;
-        private int _scanLineEnd;
-
-        // reusable buffers
-        private List<int> _scanListBuffer = new List<int>(2);
+        // reusable buffer for lines
         private MapPoint[] _mapPointBuffer = new MapPoint[4];
 
         public void Recycle(HeightMap heightMap)
@@ -42,101 +38,23 @@ namespace Mercraft.Models.Utils
 
             var elevation = start.Elevation < end.Elevation ? start.Elevation : end.Elevation;
 
-            InitializeScanLine();
-            ScanAndFill(elevation);
+            // TODO this is not good  idea to create instance of anonymous class for every small road chunk
+            SimpleScanLine.Fill(_mapPointBuffer, _size, (scanline, s, e) =>
+               Fill(scanline, s, e, elevation));
         }
 
         public void AdjustPolygon(MapPoint[] points, float elevation)
         {
-            _mapPointBuffer = new MapPoint[points.Length];
+            var mapPointBuffer = new MapPoint[points.Length];
 
             for (int i = 0; i < points.Length; i++)
             {
                 var point = points[i];
-                _mapPointBuffer[i] = GetHeightMapPoint(point.X, point.Y);
+                mapPointBuffer[i] = GetHeightMapPoint(point.X, point.Y);
             }
 
-            // we use different algorithm for random polygon cause current implementation of
-            // 
-           ScanLine.FillPolygon(_mapPointBuffer, (scanline, s, e) => 
+            ScanLine.FillPolygon(mapPointBuffer, (scanline, s, e) => 
                Fill(scanline, s, e, elevation));
-        }
-
-        private void InitializeScanLine()
-        {
-            _scanLineStart = int.MaxValue;
-            _scanLineEnd = int.MinValue;
-
-            // search start and end values
-            for (int i = 0; i < _mapPointBuffer.Length; i++)
-            {
-                var point = _mapPointBuffer[i];
-
-                if (point.Y <= 0 || point.Y >= _size)
-                    continue;
-
-                if (_scanLineEnd < point.Y)
-                    _scanLineEnd = (int)point.Y;
-                if (_scanLineStart > point.Y)
-                    _scanLineStart = (int)point.Y;
-            }
-        }
-
-        /// <summary>
-        ///     This is optimized version for particular case of algorithm for terrain
-        /// </summary>
-        private void ScanAndFill(float elevation)
-        {
-            for (int z = _scanLineStart; z <= _scanLineEnd; z++)
-            {
-                for (int i = 0; i < _mapPointBuffer.Length; i++)
-                {
-                    var currentIndex = i;
-                    var nextIndex = i == _mapPointBuffer.Length - 1 ? 0 : i + 1;
-
-                    if ((_mapPointBuffer[currentIndex].Y > z && _mapPointBuffer[nextIndex].Y > z) || // above
-                      (_mapPointBuffer[currentIndex].Y < z && _mapPointBuffer[nextIndex].Y < z)) // below
-                        continue;
-
-                    var start = _mapPointBuffer[currentIndex].X < _mapPointBuffer[nextIndex].X ?
-                          _mapPointBuffer[currentIndex]
-                        : _mapPointBuffer[nextIndex];
-
-                    var end = _mapPointBuffer[currentIndex].X < _mapPointBuffer[nextIndex].X
-                        ? _mapPointBuffer[nextIndex]
-                        : _mapPointBuffer[currentIndex];
-
-                    var x1 = start.X;
-                    var y1 = start.Y;
-                    var x2 = end.X;
-                    var y2 = end.Y;
-
-                    var d = Math.Abs(y2 - y1);
-
-                    if (Math.Abs(d) < float.Epsilon)
-                        continue;
-
-                    float tanBeta = Math.Abs(x1 - x2) / d;
-
-                    var b = Math.Abs(y1 - z);
-                    var length = b * tanBeta;
-
-                    var x = (int)(x1 + Math.Floor(length));
-
-                    if (x >= _size) x = _size - 1;
-                    if (x < 0) x = 0;
-
-                    _scanListBuffer.Add(x);
-                }
-
-                if (_scanListBuffer.Count > 1)
-                {
-                    _scanListBuffer.Sort();
-                    Fill(z, _scanListBuffer[0], _scanListBuffer[_scanListBuffer.Count - 1], elevation);
-                }
-
-                _scanListBuffer.Clear();
-            }
         }
 
         private void Fill(int line, int start, int end, float elevation)
