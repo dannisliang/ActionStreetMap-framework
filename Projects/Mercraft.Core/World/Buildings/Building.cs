61a9720692f4964e50bf6ef0f238cce58b7876a8
Ilya Builuk <ilya.builuk@gmail.com>
2014-09-19 23:43:16 +0200

    Buildings: added properties related to available data from osm

--- Projects/Mercraft.Core/World/Buildings/Building.cs
+++ Projects/Mercraft.Core/World/Buildings/Building.cs
@@ -6,22 +6,55 @@ namespace Mercraft.Core.World.Buildings
     {
         public long Id { get; set; }
 
-        public bool IsDestroyed { get; set; }
-
         public Address Address { get; set; }
-
         public IGameObject GameObject { get; set; }
-
-        public float Height { get; set; }
-
-        public float MinHeight { get; set; }
-
-        public int Levels { get; set; }
-
         public float Elevation { get; set; }
-
         public MapPoint[] Footprint { get; set; }
 
+        // NOTE OSM-available info (see details http://wiki.openstreetmap.org/wiki/Buildings)
+
         public string Type { get; set; }
+
+        #region Height specific
+
+        public float Height { get; set; }
+        public float MinHeight { get; set; }
+        public int Levels { get; set; }
+
+        #endregion
+
+        #region Appearance
+
+        public string FacadeMaterial { get; set; }
+
+
+        public string RoofMaterial { get; set; }
+        public string RoofType { get; set; }
+
+        #endregion
+
+        #region Characteristics
+
+        /// <summary>
+        ///     Indicates that the building is used as a specific shop
+        /// </summary>
+        public string Shop { get; set; }
+
+        /// <summary>
+        ///     Describes what the building is used for, for example: school, theatre, bank
+        /// </summary>
+        public string Amenity { get; set; }
+
+        /// <summary>
+        ///      Ruins of buildings
+        /// </summary>
+        public string Ruins { get; set; }
+
+        /// <summary>
+        ///     For a building which has been abandoned by its owner and is no longer maintained
+        /// </summary>
+        public string Abandoned { get; set; }
+
+        #endregion
     }
 }
